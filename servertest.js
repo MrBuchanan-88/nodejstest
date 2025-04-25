d230f7af2713884709c9939153cd9051827c92aa
Ryan Buchanan <mrrbuchanan@gmail.com>
2025-04-25 16:43:01 -0400

    Create servertest.js
    create test env

---
+++ servertest.js
@@ -0,0 +1,14 @@
+// Import the http module
+const http = require('http');
+
+// Create a server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // Success
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
